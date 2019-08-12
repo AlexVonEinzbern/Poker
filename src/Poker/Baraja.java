2b7c831bc23636c6de2fe893c7384026f3c772d9
andres <andres@andres-X555YI>
2019-08-12 14:23:38 -0500

    init

--- src/Poker/Baraja.java
+++ src/Poker/Baraja.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 
 public class Baraja {
 
-	private  ArrayList<Carta> barajaFrancesa =new ArrayList<Carta>();
-	
+	private ArrayList<Carta> barajaFrancesa = new ArrayList<Carta>();
+
 	public void crearUnaBaraja() {
-		
-		char[] palos= {'p','d','t','c'};
-		char[] colores= {'n','r','n','r'};
+
+		char[] palos = { 'p', 'd', 't', 'c' };
+		char[] colores = { 'n', 'r', 'n', 'r' };
 		String[] simbolos = { "J", "Q", "k", "A" };
 
 		// agregar cartas nuericas a la baraja francesa
@@ -25,10 +25,23 @@ public class Baraja {
 		for (int palo = 0; palo < 5; palo++) {
 
 			for (int desde = 0; desde < 4; desde++) {
-				barajaFrancesa.add(new Carta(10,simbolos[desde],palos[palo], colores[palo]));
+				barajaFrancesa.add(new Carta(10, simbolos[desde], palos[palo], colores[palo]));
 			}
 		}
-		
-		
+
+		// agregar cartas A a la baraja francesa
+
+		for (int desde = 0; desde < 5; desde++) {
+			barajaFrancesa.add(new Carta(1, simbolos[3], palos[desde], colores[desde]));
+		}
+
+	}
+
+	public Baraja() {
+		crearUnaBaraja();
+	}
+
+	public ArrayList<Carta> getBarajaFrancesa() {
+		return barajaFrancesa;
 	}
 }
