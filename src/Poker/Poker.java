082df4a8b0bc955dbcd45f693a026ae7d2996e2f
M. Alexander <m_alexander_d@outlook.es>
2019-08-12 18:23:12 -0500

    Se agrega un JLabel y JPanel.

--- src/Poker/Poker.java
+++ src/Poker/Poker.java
@@ -1,7 +1,33 @@
 package Poker;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class Poker extends JFrame {
+	JLabel imagenDeFondo = new JLabel(new ImageIcon("src/images/mesa.jpg"));
+	JPanel panel = new JPanel();
 	
+	public Poker() {
+		try {
+			initGUI();
+			//Default window config
+			this.setUndecorated(false);
+			pack();
+			this.setResizable(false);
+			this.setLocationRelativeTo(null);
+			this.setVisible(true);
+			this.setSize(1280, 720);
+			this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		} catch (Exception e) {
+			// TODO: handle exception
+			JOptionPane.showMessageDialog(null, "No se ha encontrado la imágen de fondo");
+		}
+	}
+	
+	private void initGUI() {
+		
+	}
 }
