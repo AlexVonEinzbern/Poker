64a6d159dbe4b46507fdc30fc446d613a8bdea53
andres <andres@andres-X555YI>
2019-08-12 14:21:53 -0500

    empezo esto

---
+++ src/Poker/Baraja.java
@@ -0,0 +1,34 @@
+package Poker;
+
+import java.util.ArrayList;
+
+public class Baraja {
+
+	private  ArrayList<Carta> barajaFrancesa =new ArrayList<Carta>();
+	
+	public void crearUnaBaraja() {
+		
+		char[] palos= {'p','d','t','c'};
+		char[] colores= {'n','r','n','r'};
+		String[] simbolos = { "J", "Q", "k", "A" };
+
+		// agregar cartas nuericas a la baraja francesa
+		for (int palo = 0; palo < 5; palo++) {
+
+			for (int desde = 2; desde < 11; desde++) {
+				barajaFrancesa.add(new Carta(desde, Integer.toString(desde), palos[palo], colores[palo]));
+			}
+		}
+
+		// agregar cartas J,Q,K a la baraja francesa
+
+		for (int palo = 0; palo < 5; palo++) {
+
+			for (int desde = 0; desde < 4; desde++) {
+				barajaFrancesa.add(new Carta(10,simbolos[desde],palos[palo], colores[palo]));
+			}
+		}
+		
+		
+	}
+}
---
+++ src/Poker/Carta.java
@@ -0,0 +1,19 @@
+package Poker;
+
+import javax.swing.JButton;
+
+public class Carta extends JButton {
+	
+	private int valorNumerico;
+	private  String simbolo;
+	private char palo;
+	private char color;
+	
+	public Carta(int valorNumerico,String simbolo,char palo,char color) {
+		this.valorNumerico=valorNumerico;
+		this.simbolo=simbolo;
+		this.palo=palo;
+		this.color=color;
+	}
+
+}
