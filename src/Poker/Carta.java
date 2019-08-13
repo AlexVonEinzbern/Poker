10eebcb1c186e46a3c7001d1c1e2ed176284b72a
M. Alexander <m_alexander_d@outlook.es>
2019-08-12 22:15:12 -0500

    Comentarios de la clase Carta.

--- src/Poker/Carta.java
+++ src/Poker/Carta.java
@@ -3,14 +3,41 @@ package Poker;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
+/**
+ * Hace referencia a una carta del juego Poker.
+ * @author
+ *
+ */
 public class Carta extends JButton {
 
+	/**
+	 * Valor numérico de la carta.
+	 */
 	private int valorNumerico;
+	
+	/**
+	 * Símbolo de la carta.
+	 */
 	private String simbolo;
+	
+	/**
+	 * Palo de la carta.
+	 */
 	private String palo;
+	
+	/**
+	 * Color de la carta.
+	 */
 	private String color;
 	
 	
+	/**
+	 * Constructor de la clase Carta.
+	 * @param valorNumerico
+	 * @param simbolo
+	 * @param palo
+	 * @param color
+	 */
 	public Carta(int valorNumerico,String simbolo,String palo,String color) {
 		this.valorNumerico=valorNumerico;
 		this.simbolo=simbolo;
